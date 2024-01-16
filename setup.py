8f32467fc4dc099aa937245ba5ab50a3f4811045
pizzaolive <git_projects@protonmail.com>
2024-01-16 16:14:42 +0000

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='ant_upload_checker',
-   version='1.0',
+   version='1.1',
    description='Check if films in input directory are already uploaded to ANT',
    author='pizzaolive',
    packages=['ant_upload_checker'], 
