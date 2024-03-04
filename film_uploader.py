bb8bdd130d525125fdeda338ba2306c37f054305
pizzaolive <git_projects@protonmail.com>
2024-03-04 19:10:10 +0000

    Create placeholder files

---
+++ film_uploader.py
@@ -0,0 +1,17 @@
+import logging
+import requests
+from ant_upload_checker.parameters import API_KEY
+
+
+class FilmUploader:
+    def __init__(self,film_path,api_key):
+        self.film_path = film_path
+        self.api_key = api_key
+
+    def create_torrent_file():
+
+        return None
+
+    def upload_film():
+
+        return None
