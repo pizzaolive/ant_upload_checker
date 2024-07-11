bd56db443358f67488db0edb1853e5dc27811b76
pizzaolive <git_projects@protonmail.com>
2024-07-11 21:18:47 +0100

    Add initial inquirer code

--- ant_upload_checker/main.py
+++ ant_upload_checker/main.py
@@ -1,5 +1,5 @@
 import logging
-from ant_upload_checker.setup import setup_logging
+from ant_upload_checker.setup import setup_logging, get_user_information
 from ant_upload_checker.film_processor import FilmProcessor
 from ant_upload_checker.film_searcher import FilmSearcher
 from ant_upload_checker.output import write_film_list_to_csv
@@ -9,6 +9,8 @@ from ant_upload_checker.parameters import INPUT_FOLDERS, API_KEY, OUTPUT_FOLDER
 def main():
     setup_logging()
     logging.info("Starting ANT upload checker...")
+
+    get_user_information()
 
     films = FilmProcessor(INPUT_FOLDERS, OUTPUT_FOLDER)
     film_file_paths = films.get_film_file_paths()
--- ant_upload_checker/setup.py
+++ ant_upload_checker/setup.py
@@ -1,5 +1,29 @@
 import logging
+import inquirer
+from pathlib import Path
+from dotenv import set_key
+from typing import List
 
 
 def setup_logging():
     logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+
+def get_user_information() -> List[str]:
+    prompts = [inquirer.Text("api_key", message="Please enter your API key from ANT")]
+
+    responses = inquirer.prompt(prompts)
+
+    save_user_info_to_dot_env(responses)
+
+    return responses
+
+
+def save_user_info_to_dot_env(user_info: List[str]) -> None:
+    env_file_path = Path(".env").resolve()
+    env_file_path.touch()
+    set_key(
+        dotenv_path=env_file_path,
+        key_to_set="API_KEY",
+        value_to_set=user_info["api_key"],
+    )
