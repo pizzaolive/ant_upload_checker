ac761229e74dcee885ce620577f9dbf3cfd78899
pizzaolive <git_projects@protonmail.com>
2024-07-17 20:03:31 +0100

    Improve user prompts and add GUI

--- ant_upload_checker/setup.py
+++ ant_upload_checker/setup.py
@@ -2,21 +2,57 @@ import logging
 import inquirer
 from pathlib import Path
 from dotenv import set_key
-from typing import List
+import tkinter
+import tkfilebrowser
+from typing import List, Tuple
 
 
 def setup_logging():
     logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 
+def check_if_user_wants_to_override_env_file():
+    question = [
+        inquirer.List(
+            "choice",
+            message="An API key was already found, do you want to overwrite this?",
+            choices=["Yes", "No"],
+            default="No",
+        )
+    ]
+    response = inquirer.prompt(question)
+
+    if response["choice"] == "Yes":
+        return True
+    return False
+
+
 def get_user_information() -> List[str]:
-    prompts = [inquirer.Text("api_key", message="Please enter your API key from ANT")]
+    env_file_path = Path(".env")
+    if not env_file_path.is_file() or check_if_user_wants_to_override_env_file():
+        get_user_input_api_key()
 
-    responses = inquirer.prompt(prompts)
+    get_input_folders()
 
-    save_user_info_to_dot_env(responses)
 
-    return responses
+def get_input_folders() -> Tuple[str]:
+    root = tkinter.Tk()
+    root.withdraw()
+    logging.info("Please select the directory containing your films in the dialog box")
+    directories = tkfilebrowser.askopendirnames(
+        title="Please select where your films are stored"
+    )
+    root.destroy()
+
+    return directories
+
+
+def get_user_input_api_key() -> List[str]:
+    prompt = [inquirer.Text("api_key", message="Please enter your API key from ANT")]
+
+    user_api_key = inquirer.prompt(prompt)
+
+    save_user_info_to_dot_env(user_api_key)
 
 
 def save_user_info_to_dot_env(user_info: List[str]) -> None:
