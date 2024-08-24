080edd6568f9b253c3f4f0c2e29418862c126f0b
pizzaolive <git_projects@protonmail.com>
2024-08-24 16:56:02 +0100

    Add error handling to tkinter GUI

--- ant_upload_checker/directory_selector.py
+++ ant_upload_checker/directory_selector.py
@@ -1,20 +1,24 @@
 import tkinter as tk
 from tkinter import filedialog
-from pathlib import Path
 import logging
 
 
 class DirectorySelector:
     def __init__(self) -> None:
         self.root = tk.Tk()
+        self.root.protocol("WM_DELETE_WINDOW", self.root.quit)
 
     def add_folder(self) -> None:
         """
         Open a dialog to select a given directory, add it to the listbox
         """
-        folder = filedialog.askdirectory(title="Select Folder")
+        folder = filedialog.askdirectory(title="Select folder containing films")
         if folder:
-            self.folder_listbox.insert(tk.END, folder)
+            current_folder_selection = list(self.folder_listbox.get(0, tk.END))
+            if folder in current_folder_selection:
+                logging.info("Folder has already been selected, skipping")
+            else:
+                self.folder_listbox.insert(tk.END, folder)
 
     def select_input_directories(self) -> list[str]:
         """
@@ -28,21 +32,28 @@ class DirectorySelector:
         self.folder_listbox = tk.Listbox(
             self.root,
             selectmode=tk.SINGLE,
-            width=60,
+            width=100,
             height=15,
         )
 
         self.root.title("Select one or more input directories containing your films")
         self.folder_listbox.pack(pady=20)
 
-        add_button = tk.Button(self.root, text="Add Folder", command=self.add_folder)
+        add_button = tk.Button(self.root, text="Add folder", command=self.add_folder)
         add_button.pack(pady=10)
 
-        save_button = tk.Button(self.root, text="Save Folders", command=self.root.quit)
+        save_button = tk.Button(
+            self.root, text="Finish folder selection", command=self.root.quit
+        )
         save_button.pack(pady=10)
-
         self.root.mainloop()
+
         input_directories = list(self.folder_listbox.get(0, tk.END))
+
+        if not input_directories:
+            raise ValueError(
+                "No folders were selected in the dialog box, please re-run"
+            )
 
         # Hide but don't close
         self.root.withdraw()
@@ -60,8 +71,10 @@ class DirectorySelector:
             title="Please select your output directory",
         )
 
-        # Hide but don't close
-        self.root.withdraw()
+        if not output_directory:
+            raise ValueError(
+                "No output folder was selected in the dialog box, please re-run"
+            )
 
         return output_directory
 
